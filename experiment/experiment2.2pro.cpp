a3c627c9b3631f6ea2ffee46b3204d5f3b7282d9
linzui <yangminghe20@gmail.com>
2023-10-23 17:40:25 +0800

    实验二

--- experiment/experiment2.2pro.cpp
+++ experiment/experiment2.2pro.cpp
@@ -3,26 +3,32 @@
 
 int n;
 int *coins;
+int *value;
 
 int getValue(int amount) {
     int min = 0x7fffffff;
     for (int i = 0; i < n; i++) {
         if (amount == coins[i]) {
+            value[amount] = 1;
             return 1;
         } else if (amount > coins[i]) {
+            if (value[amount] > 0)
+                return value[amount];
             int var = getValue(amount - coins[i]) + 1;
             if (var == 0)
                 continue;
             min = min < var ? min : var;
         }
     }
-    return min == 0x7fffffff ? -1 : min;
+    value[amount] = min == 0x7fffffff ? -1 : min;
+    return value[amount];
 }
 
 int main() {
     int amount;
     scanf("%d %d", &n, &amount);
     coins = (int *)calloc(n, sizeof(int));
+    value = (int *)calloc(amount, sizeof(int));
     for (int i = 0; i < n; i++) {
         scanf("%d", &coins[i]);
     }
